3d7e948014b85d0d6f0d645e6c51972f39384630
asluis <asluisinfo@gmail.com>
2021-01-26 22:40:20 -0800

    Code upload along some additions

---
+++ Movie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Movie+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Luis Alvarez on 1/26/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
